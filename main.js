a84ddbdcafc1a4e6dd2b4994e1a5815cf9b61be7
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-04-27 21:02:06 -0400

    adds grocery list functionality before refactor

--- main.js
+++ main.js
@@ -1,64 +1,173 @@
-// SHOPPING LIST OBJECT
+// // SHOPPING LIST OBJECT
+
+// const shoppingList = {
+//   items: [], // ARRAY TO STORE ITEMS
+//   htmlShoppingList: null,
+//   htmlAddItemForm: null,
+//   htmlInputField: null,
+//   initiate: function () {
+//     // SET'S UP THE PAGE
+//     // OBTAINS HTML ELEMENTS
+//     shoppingList.htmlShoppingList = documentQuerySelector("shop-list");
+//     shoppingList.htmlAddItemForm = documentQuerySelector("shop-add");
+//     shoppingList.htmlInputField = documentQuerySelector("shop-item");
+//     shoppingList.htmlAddItemForm.addEventListener("submit", shoppingList.add);
+
+//     // RESTORE PREVIOUS SHOPPING LIST
+//     if (localStorage.items == undefined) {
+//       localStorage.items = "[]";
+//     }
+//     shoppingList.items = JSON.parse(localStorage.items);
+
+//     // EXECUTES CODE INSIDE BLOCK .DRAW FUNCTION
+//     shoppingList.draw();
+//   },
+
+//   // ADD A NEW ITEM TO LIST
+//   add: function (event) {
+//     // PREVENT FORM SUBMIT
+//     event.preventDefault();
+
+//     // ADD NEW ITEM TO LIST
+//     shoppingList.items.push({
+//       name: shoppingList.htmlInputField.value,
+//       done: false,
+//     });
+//     // LOAD HTML SHOPPING LIST
+//     shoppingList.htmlInputField.value = "";
+//     shoppingList.save();
+
+//     // LOADS HTML SHOPPING LIST
+//     shoppingList.draw();
+//   }
+
+//   // LOOPS THROUGH SHOPPING LIST AND CREATES HTML
+//     draw: function () {
+//     // ZERO OUT LIST
+//     shoppingList.htmlShoppingList.innerHTML = "";
+//     if (shoppingList.items.length > 0) {
+//       let row, name, deleteButton, okayButton;
+//       for (let input in shoppingList.items) {
+//         // ITEM ROW -> DIV FOR INPUT
+//         row = document.createElement("div");
+//         row.className = "item-row";
+//         shoppingList.htmlShoppingList.appendChild(row);
+
+//         // ITEM NAME
+//         name = document.createElement("div");
+//         name.className = "item-name";
+//         name.innerHTML = shoppingList.items[i].name;
+//         if (shoppingList.items[i].done) {
+//           name.classList.add("item-got");
+//         }
+//         row.appendChild(name);
+
+//         // CREATES DELETE BUTTON
+//         deleteButton = document.createElement("input");
+//         deleteButton.className = "item-del";
+//         deleteButton.type = "button";
+//         deleteButton.value = "Delete";
+//         deleteButton.dataset.id = input;
+//         deleteButton.addEventListener("click", shoppingList.delete);
+//         row.appendChild(deleteButton);
+
+//         // COMPLETED ? NOT YET COMPLETED -> BUTTON
+//         okayButton = document.createElement("input");
+//         okayButton = className = "item-ok";
+//         okayButton.type = "button";
+//         // TERNARY
+//         okayButton.value = shoppingList.items[i].done ? "Not Yet" : "Got It";
+//         okayButton.dataset.id = input;
+//         okayButton.addEventListener("click", shoppingList.toggle);
+//         row.appendChild(okayButton);
+//       }
+//     }
+//   }
+
+// // SAVES SHOPPING LIST INTO LOCAL STORAGE
+
+//   save: function () {
+//     if (localStorage.items == undefined) {
+//       // EMPTY ARRAY
+//       localStorage.items = "[]";
+//     }
+//     // SAVES INTO LOCAL STORAGE
+//     localStorage.items = JSON.stringify(shoppingList.items);
+//   },
+
+// // DELETE SELECTED ITEM -> UPDATES LOCAL STORAGE -> UPDATED HTML
+
+//   delete: function () {
+//     if (confirm("Remove selected item?")) {
+//       shoppingList.items.splice(this.dataset.id, 1);
+//       shoppingList.save();
+//       shoppingList.draw();
+//     }
+//   },
+
+//   toggle: function () {
+//     let id = this.dataset.id;
+//     shoppingList.items[id].done = !shoppingList.items[id].done;
+//     shoppingList.save();
+//     shoppingList.draw();
+//   },
+// };
+
+// window.addEventListener("DOMContentLoaded", shoppingList.initiate);
 
 const shoppingList = {
-  items: [], // ARRAY TO STORE ITEMS
+  items: [], // ARRAY TO SAVE RAW DATA
   htmlShoppingList: null,
   htmlAddItemForm: null,
   htmlInputField: null,
   initiate: function () {
-    // SET'S UP THE PAGE
-    // OBTAINS HTML ELEMENTS
-    shoppingList.htmlShoppingList = documentQuerySelector("shop-list");
-    shoppingList.htmlAddItemForm = documentQuerySelector("shop-add");
-    shoppingList.htmlInputField = documentQuerySelector("shop-item");
+    // OBTAINS HTML ELEMENTS -> SET'S UP PAGE
+    shoppingList.htmlShoppingList = document.getElementById("shop-list");
+    shoppingList.htmlAddItemForm = document.getElementById("shop-add");
+    shoppingList.htmlInputField = document.getElementById("shop-item");
     shoppingList.htmlAddItemForm.addEventListener("submit", shoppingList.add);
 
-    // RESTORE PREVIOUS SHOPPING LIST
+    // RESTORES PREVIOUS SHOPPING LIST
     if (localStorage.items === undefined) {
       localStorage.items = "[]";
     }
     shoppingList.items = JSON.parse(localStorage.items);
 
-    // EXECUTES CODE INSIDE BLOCK .DRAW FUNCTION
+    // EXECUTES INSIDE BLOCK .DRAW() FUNCTION
     shoppingList.draw();
   },
-};
-window.addEventListener("DOMContentLoaded", shoppingList.initiate);
 
-const shoppingList = {
-  // ADD A NEW ITEM TO LIST
+  // ADDS NEW ITEM TO LIST
   add: function (event) {
-    // PREVENT FORM SUBMIT
+    // PREVENT FORM RE-SUBMISSION
     event.preventDefault();
 
-    // ADD NEW ITEM TO LIST
+    // ADDS ITEM
     shoppingList.items.push({
-      name: shoppingList.htmlInputField.value,
-      done: false,
+      name: shoppingList.htmlInputField.value, // NAME
+      done: false, // TRUE = "GOT IT" | FALSE = FOR "NOT YET"
     });
-    // LOAD HTML SHOPPING LIST
+
+    // UPDATES LOCAL STORAGE
     shoppingList.htmlInputField.value = "";
     shoppingList.save();
 
-    // LOADS HTML SHOPPING LIST
+    // EXECUTE CODE
     shoppingList.draw();
   },
-};
 
-const shoppingList = {
-  // LOOPS THROUGH SHOPPING LIST AND CREATES HTML
+  // (C) DRAW THE HTML SHOPPING LIST
   draw: function () {
-    // ZERO OUT LIST
     shoppingList.htmlShoppingList.innerHTML = "";
     if (shoppingList.items.length > 0) {
-      let row, name, deleteButton, okayButton;
-      for (let input in shoppingList.items) {
-        // ITEM ROW -> DIV FOR INPUT
+      let row, name, delbtn, okbtn;
+      for (let i in shoppingList.items) {
+        // CREATES ITEM ROW
         row = document.createElement("div");
         row.className = "item-row";
         shoppingList.htmlShoppingList.appendChild(row);
 
-        // ITEM NAME
+        // CREATES ITEM NAME
         name = document.createElement("div");
         name.className = "item-name";
         name.innerHTML = shoppingList.items[i].name;
@@ -67,26 +176,50 @@ const shoppingList = {
         }
         row.appendChild(name);
 
-        // CREATES DELETE BUTTON
+        // DELETE BUTTON
         deleteButton = document.createElement("input");
         deleteButton.className = "item-del";
         deleteButton.type = "button";
         deleteButton.value = "Delete";
-        deleteButton.dataset.id = input;
+        deleteButton.dataset.id = i;
         deleteButton.addEventListener("click", shoppingList.delete);
         row.appendChild(deleteButton);
 
-        // COMPLETED / NOT YET COMPLETED -> BUTTON
+        // COMPLETED ? NOT YET : IN CART -> BUTTON
         okayButton = document.createElement("input");
-        okayButton = className = "item-ok";
+        okayButton.className = "item-ok";
         okayButton.type = "button";
-        okayButton.value = shoppingList.items[i].done ? "Not Yet" : "Got It";
-        okayButton.dataset.id = input;
+        okayButton.value = shoppingList.items[i].done ? "IN CART" : "NOT YET";
+        okayButton.dataset.id = i;
         okayButton.addEventListener("click", shoppingList.toggle);
         row.appendChild(okayButton);
       }
     }
   },
+
+  // UPDATES SHOPPING LIST TO LOCAL STORAGE
+  save: function () {
+    if (localStorage.items === undefined) {
+      localStorage.items = "[]";
+    }
+    localStorage.items = JSON.stringify(shoppingList.items);
+  },
+
+  // DELETES ITEM FROM LIST 
+  delete: function () {
+    if (confirm("Remove selected item?")) {
+      shoppingList.items.splice(this.dataset.id, 1);
+      shoppingList.save();
+      shoppingList.draw();
+    }
+  },
+
+  // TOGGLE FUNCTION FOR IN CART ? NOT YET
+  toggle: function () {
+    let id = this.dataset.id;
+    shoppingList.items[id].done = !shoppingList.items[id].done;
+    shoppingList.save();
+    shoppingList.draw();
+  },
 };
-
-
+window.addEventListener("DOMContentLoaded", shoppingList.initiate);
